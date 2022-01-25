43f93222c779d023615acd1a274735339eee6d3b
Stefan Sommer <sommer@di.ku.dk>
2022-01-25 20:50:33 +0100

    product sde and conditioning on product diagonal

---
+++ src/stochastics/diagonal_conditioning.py
@@ -0,0 +1,62 @@
+## This file is part of Jax Geometry
+#
+# Copyright (C) 2021, Stefan Sommer (sommer@di.ku.dk)
+# https://bitbucket.org/stefansommer/jaxgeometry
+#
+# Jax Geometry is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Jax Geometry is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Theano Geometry. If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+from src.setup import *
+from src.utils import *
+
+def initialize(M,sde_product,chart_update_product,integrator=integrator_ito,T=1):
+    """ diagonally conditioned product diffusions """
+
+    def sde_diagonal(c,y):
+        if M.do_chart_update is None:
+            t,x,chart,T,*cy = c
+        else:
+            t,x,chart,T,ref_chart,*cy = c
+        dt,dW = y
+        
+        (det,sto,X,*dcy) = sde_product((t,x,chart,*cy),y)
+
+        if M.do_chart_update is None:
+            xref = x
+        else:
+            xref = jax.vmap(lambda x,chart: M.update_coords((x,chart),ref_chart)[0],0)(x,chart)
+        m = jnp.mean(xref,0) # mean
+        href = jax.lax.cond(t<T-dt/2,
+                 lambda _: (m-xref)/(T-t),
+                 lambda _: jnp.zeros_like(det),
+                 None)
+        if M.do_chart_update is None:
+            h = href
+        else:
+            h = jax.vmap(lambda xref,x,chart,h: M.update_vector((xref,ref_chart),x,chart,h),0)(xref,x,chart,href)
+        
+        # jnp.tensordot(X,h,(2,1))
+        if M.do_chart_update is None:
+            return (det+h,sto,X,0.,*dcy)
+        else:
+            return (det+h,sto,X,0.,jnp.zeros_like(ref_chart),*dcy)
+    
+    M.sde_diagonal = sde_diagonal
+    M.chart_update_diagonal = chart_update_product
+    if M.do_chart_update is None:
+        M.diagonal = jit(lambda x,dts,dWt: integrate_sde(sde_diagonal,integrator,M.chart_update_diagonal,x[0],x[1],dts,dWt,jnp.sum(dts))[0:3])
+    else:
+        M.diagonal = jit(lambda x,dts,dWt,ref_chart: integrate_sde(sde_diagonal,integrator,M.chart_update_diagonal,x[0],x[1],dts,dWt,jnp.sum(dts),ref_chart)[0:3])
+
---
+++ src/stochastics/sde_product.py
@@ -0,0 +1,45 @@
+## This file is part of Jax Geometry
+#
+# Copyright (C) 2021, Stefan Sommer (sommer@di.ku.dk)
+# https://bitbucket.org/stefansommer/jaxgeometry
+#
+# Jax Geometry is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Jax Geometry is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Theano Geometry. If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+from src.setup import *
+from src.utils import *
+
+def initialize(M,sde,chart_update,integrator=integrator_ito):
+    """ product diffusions """
+
+    def sde_product(c,y):
+        t,x,chart,*cy = c
+        dt,dW = y
+        
+        (det,sto,X,*dcy) = jax.vmap(lambda x,chart,dW: sde((t,x,chart,*cy),(dt,dW)),0)(x,chart,dW)
+
+        return (det,sto,X,*dcy)
+    
+    # note: y is not parametrized on product and thus cannot be updated by chart update
+    def chart_update_product(x,chart,y):
+        if M.do_chart_update is None:
+            return (x,chart,*y)
+
+        return (*jax.vmap(lambda x,chart: chart_update(x,chart,y),0)(x,chart),*y)
+    
+    M.sde_product = sde_product
+    M.chart_update_product = chart_update_product
+    M.product = jit(lambda x,dts,dWs: integrate_sde(sde_product,integrator,chart_update_product,x[0],x[1],dts,dWs))
+
